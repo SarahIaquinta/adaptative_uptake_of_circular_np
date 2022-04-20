0ab1a6904cc5d8b39d77fbc167170b4729ae2bee
SarahIaquinta <sarah.r.iaquinta@gmail.com>
2022-04-20 15:25:13 +0200

    scripts to create and validate Kriging metamodels|

---
+++ metamodel/metamodel_creation.py
@@ -0,0 +1,311 @@
+# adding folders model and sensitivity_analysis to the system path
+import os
+import sys
+from os import listdir
+from os.path import isfile, join
+
+path = os.path.dirname(os.path.abspath(__file__))
+path_model = path + "/../model"
+existing_filenames = {f for f in listdir(path_model) if isfile(join(path, f))}
+sys.path.insert(1, path_model)
+path_sensitivity_analysis = path + "/../sensitivity_analysis"
+sys.path.insert(1, path_sensitivity_analysis)
+
+
+import numpy as np
+import openturns as ot
+
+ot.Log.Show(ot.Log.NONE)
+
+import utils_metamodel
+
+
+class DataPreSetting:
+    """
+    A class that performs the presets on the dataset to compute the metamodel
+
+    Attributes:
+        ----------
+        filename: string
+            name of the .txt file from which the data will be extracted
+        training_amount : float
+            proportion (between 0 and 1) of the initial data used for training (the remaining data
+            are used for testing)
+
+    Methods:
+        -------
+        shuffle_dataset_from_datafile(self):
+            imports and shuffles the initial dataset
+        extract_training_data_from_shuffled_dataset(self, shuffled_sample):
+            extracts a proportion (training_amount) of the shuffled_data to generate the training
+            dataset
+
+    """
+
+    def __init__(self, filename, training_amount):
+        """
+        Constructs all the necessary attributes for the DataPreSetting object.
+
+        Parameters:
+            ----------
+            filename: string
+                name of the .txt file from which the data will be extracted
+            training_amount : float
+                proportion (between 0 and 1) of the initial data used for training (the remaining
+                data are used for testing)
+
+        Returns:
+            -------
+            None
+        """
+
+        self.filename = path + "/" + filename
+        self.training_amount = training_amount
+
+    def shuffle_dataset_from_datafile(self):
+        """
+        imports and shuffles the initial dataset using Openturns library
+
+        Parameters:
+            ----------
+            None
+
+        Returns:
+            -------
+            sample: ot class
+                shuffled input dataset
+
+        """
+        sample = ot.Sample.ImportFromTextFile(self.filename, "\t", 0)
+        np.random.shuffle(sample)
+        return sample
+
+    def extract_training_data_from_shuffled_dataset(self, shuffled_sample):
+        """
+        extracts a proportion (training_amount) of the shuffled_data to train the metamodel
+
+        Parameters:
+            ----------
+            shuffled_sample : ot class
+
+
+        Returns:
+            -------
+            shuffled_training_input_sample: ot class
+                part of the input data used to train the metamodel
+            shuffled_training_output_sample: ot class
+                output data corresponding to the shuffled_training_input_sample
+
+        """
+        datasize, _ = np.shape(shuffled_sample)
+        datasize = 1000
+        shuffled_training_input_sample = ot.Sample(shuffled_sample[: int(self.training_amount * datasize), 0:6])
+        shuffled_training_output_sample = ot.Sample(shuffled_sample[: int(self.training_amount * datasize), -1:])
+        return shuffled_training_input_sample, shuffled_training_output_sample
+
+    def extract_testing_data(self, shuffled_sample):
+        """
+        extracts a proportion (1 - training_amount) of the shuffled_data to test the metamodel
+
+        Parameters:
+            ----------
+            shuffled_sample : ot class
+
+
+        Returns:
+            -------
+            shuffled_testing_input_sample: ot class
+                part of the input data used to test the metamodel
+            shuffled_testing_output_sample: ot class
+                output data corresponding to the shuffled_testing_input_sample
+
+        """
+        datasize, _ = np.shape(shuffled_sample)
+        datasize = 1000
+        shuffled_testing_input_sample = ot.Sample(
+            shuffled_sample[int(self.training_amount * datasize + 1) : datasize, 0:6]
+        )
+        shuffled_testing_output_sample = ot.Sample(
+            shuffled_sample[int(self.training_amount * datasize + 1) : datasize, -1:]
+        )
+        return shuffled_testing_input_sample, shuffled_testing_output_sample
+
+
+class MetamodelCreation:
+    def __init__(self, input_sample_training, output_sample_training):
+        """
+        Constructs all the necessary attributes for the MetamodelCreation object.
+
+        Parameters:
+            ----------
+            input_sample_training: ot class (array)
+                sample of input parameters for training, generated in the DataPreSetting class
+            output_sample_training: ot class (array)
+                sample of output parameters for training, generated in the DataPreSetting class
+
+        Returns:
+            -------
+            None
+        """
+
+        self.input_sample_training = input_sample_training
+        self.output_sample_training = output_sample_training
+        _, self.dimension = np.shape(self.input_sample_training)
+
+    def create_kriging_algorithm(self):
+        """
+        computes the Kriging Algorithm of the Openturns library
+
+        Parameters:
+            ----------
+            None
+
+
+        Returns:
+            -------
+            kriging_algorithm: ot class
+                Kriging algorithm from the Openturns library
+
+        """
+        basis = ot.Basis(0)
+        cov = ot.SphericalModel(self.dimension)
+        kriging_algorithm = ot.KrigingAlgorithm(self.input_sample_training, self.output_sample_training, cov, basis)
+        return kriging_algorithm
+
+
+class MetamodelPostTreatment:
+    def __init__(self):
+        """
+        Constructs all the necessary attributes for the MetamodelPostTreatment object.
+
+        Parameters:
+            ----------
+            None
+
+        Returns:
+            -------
+            None
+        """
+
+    def run_algorithm(self, algorithm):
+        """
+        runs the algorithm
+
+        Parameters:
+            ----------
+            algorithm: ot class
+                algorithm (kriging), output of the methods which creates the respective algorithm
+
+
+        Returns:
+            -------
+            Nothing
+
+        """
+        algorithm.run()
+
+    def extract_results_from_algorithm(self, algorithm):
+        """
+        gets the results from the algorithm
+
+        Parameters:
+            ----------
+            algorithm: ot class
+                algorithm (kriging), output of the methods which creates the respective algorithm
+
+
+        Returns:
+            -------
+            results: ot class
+                result from the algorithm, computing following a method from the Openturns library
+
+        """
+        results = algorithm.getResult()
+        return results
+
+    def get_metamodel_from_results_algo(self, results_from_algo):
+        """
+        gets the metamodel from the algorithm's results
+
+        Parameters:
+            ----------
+            results_from_algo: ot class
+                result from the algorithm, computing following a method from the Openturns library
+                output of the method self.extract_results_from_algorithm(algorithm)
+
+
+        Returns:
+            -------
+            metamodel: ot class
+                metamodel computed by the algorithm. contains the information necessary to use the
+                metamodel
+
+        """
+        metamodel = results_from_algo.getMetaModel()
+        return metamodel
+
+    def get_errors_from_metamodel(self, results_from_algo):
+        """
+        gets the errors from the metamodel (obtained according to the results_from_algo)
+
+        Parameters:
+            ----------
+            results_from_algo: ot class
+                result from the algorithm, computing following a method from the Openturns library
+
+
+        Returns:
+            -------
+            residual: ot class (array)
+                residual of the metamodel
+            relative_error: ot class (array)
+                relative_error of the metamodel
+
+        """
+
+        residual = results_from_algo.getResiduals()
+        relative_error = results_from_algo.getRelativeErrors()
+        return residual, relative_error
+
+
+def metamodel_creation_routine_kriging(datapresetting, metamodelcreation, metamodelposttreatment, shuffled_sample):
+    """
+    Runs the routine to create a metamodel:
+        1 - imports the data that will be used to create the metamodel 2 - creates the metamodel 3
+        - exports the metamodel in a .pkl file
+
+    Parameters:
+        ----------
+        datapresetting: class
+            A class that performs the presets on the dataset to compute the metamodel
+        metamodelcreation: class
+            A class that contains the routines to create a metamodel
+        metamodelposttreatment: class
+            A class that contains the routines to extract the algorithms from the metamodel
+        shuffled_sample: ot class
+            sample that was used to create the metamodel
+
+    Returns:
+        -------
+        Nothing
+
+    """
+    kri = metamodelcreation.create_kriging_algorithm()
+    metamodelposttreatment.run_algorithm(kri)
+    results_from_kri = metamodelposttreatment.extract_results_from_algorithm(kri)
+    complete_pkl_filename_Kriging = utils_metamodel.create_pkl_name("Kriging", datapresetting.training_amount)
+    utils_metamodel.export_metamodel_and_data_to_pkl(shuffled_sample, results_from_kri, complete_pkl_filename_Kriging)
+
+
+if __name__ == "__main__":
+    filename_qMC_Sobol = "dataset_for_metamodel_creation.txt"
+    training_amount_list = [0.7]  # , 0.75, 0.8, 0.85, 0.9, 0.95]
+    metamodelposttreatment = MetamodelPostTreatment()
+    for training_amount in training_amount_list:
+        datapresetting = DataPreSetting(filename_qMC_Sobol, training_amount)
+        shuffled_sample = datapresetting.shuffle_dataset_from_datafile()
+        input_sample_training, output_sample_training = datapresetting.extract_training_data_from_shuffled_dataset(
+            shuffled_sample
+        )
+        metamodelcreation = MetamodelCreation(input_sample_training, output_sample_training)
+        metamodel_creation_routine_kriging(datapresetting, metamodelcreation, metamodelposttreatment, shuffled_sample)
---
+++ metamodel/metamodel_validation.py
@@ -0,0 +1,149 @@
+# adding folders model and sensitivity_analysis to the system path
+import os
+import sys
+
+path = os.path.dirname(os.path.abspath(__file__))
+path_model = path + "/../model"
+sys.path.insert(1, path_model)
+path_sensitivity_analysis = path + "/../sensitivity_analysis"
+sys.path.insert(1, path_sensitivity_analysis)
+
+import openturns as ot
+import openturns.viewer as viewer
+
+ot.Log.Show(ot.Log.NONE)
+
+import utils_metamodel
+from matplotlib import pylab as plt
+from metamodel_creation import DataPreSetting, MetamodelPostTreatment
+
+
+class MetamodelValidation:
+    def __init__(self):
+        """
+        Constructs all the necessary attributes for the MetamodelValidation object.
+
+        Parameters:
+            ----------
+            None
+        Returns:
+            -------
+            None
+        """
+
+    def validate_metamodel_with_test(self, inputTest, outputTest, metamodel):
+        """
+        Constructs a metamodel validator class (from the Openturns library).
+
+        Parameters:
+            ----------
+            metamodel: class
+                metamodel object (from the OpenTurns library)
+            inputTest: class (array)
+                part of the dataset (input variables of the model) that will be used to validate
+                the metamodel
+            outputTest: class (array)
+                part of the dataset (output variables of the model) that will be used to validate
+                the metamodel
+
+        Returns:
+            -------
+            metamodel_validator: class
+                tool from the Openturns library used to validate a metamodel
+        """
+        metamodel_validator = ot.MetaModelValidation(inputTest, outputTest, metamodel)
+        return metamodel_validator
+
+    def compute_Q2(self, metamodel_validator):
+        """
+        Computes the predictivity factor of the model.
+
+        Parameters:
+            ----------
+            metamodel_validator: class
+                tool from the Openturns library used to validate a metamodel
+
+        Returns:
+            -------
+            Q2: class (array)
+                predictivity factor
+        """
+
+        Q2 = metamodel_validator.computePredictivityFactor()
+        return Q2
+
+    def plot_prediction_vs_true_value(self, metamodel_validator, type_of_metamodel, training_amount):
+        """
+        Plots the prediction (from metamodel) vs true value (of the model).
+
+        Parameters:
+            ----------
+            metamodel_validator: class
+                tool from the Openturns library used to validate a metamodel
+            type_of_metamodel: string
+                name of the metamodel that has been computed. Possible values :
+                    "Kriging"
+
+        Returns:
+            -------
+            Nothing
+
+        """
+        graph = metamodel_validator.drawValidation()
+        Q2 = metamodel_validator.computePredictivityFactor()
+        graph.setTitle(
+            type_of_metamodel
+            + " metamodel validation (training amount = "
+            + str(training_amount)
+            + ") ; \n Q2 = "
+            + str(Q2)
+        )
+        view = viewer.View(graph)
+        plt.xlabel(r"true value of $\tilde{f}$ (from model)")
+        plt.ylabel(r"predicted value of $\tilde{f}$ (from metamodel)")
+        plt.show()
+
+
+def metamodel_validation_routine(
+    datapresetting, metamodelposttreatment, metamodelvalidation, type_of_metamodel, training_amount
+):
+    """
+    Runs the routine to validate a metamodel:
+        1 - imports the metamodel from a .pkl file 2 - compares the true vs predicted value of the
+        metamodel
+
+    Parameters:
+        ----------
+        datapresetting: class
+            A class that performs the presets on the dataset to compute the metamodel
+        metamodel_validator: class
+            tool from the Openturns library used to validate a metamodel
+        type_of_metamodel: string
+            name of the metamodel that has been computed. Possible values :
+                "Kriging"
+        training_amount: float (between 0 and 1)
+            amount of the data that is used to train the metamodel
+
+    Returns:
+        -------
+        Nothing
+
+    """
+    complete_pkl_filename = utils_metamodel.create_pkl_name(type_of_metamodel, training_amount)
+    shuffled_sample, results_from_algo = utils_metamodel.extract_metamodel_and_data_from_pkl(complete_pkl_filename)
+    metamodel = metamodelposttreatment.get_metamodel_from_results_algo(results_from_algo)
+    inputTest, outputTest = datapresetting.extract_testing_data(shuffled_sample)
+    metamodel_validator = metamodelvalidation.validate_metamodel_with_test(inputTest, outputTest, metamodel)
+    metamodelvalidation.plot_prediction_vs_true_value(metamodel_validator, type_of_metamodel, training_amount)
+
+
+if __name__ == "__main__":
+    filename_qMC_Sobol = "dataset_for_metamodel_creation.txt"
+    metamodelposttreatment = MetamodelPostTreatment()
+    metamodelvalidation = MetamodelValidation()
+    training_amount_list = [0.7]  # , 0.75, 0.8, 0.85, 0.9, 0.95]
+    for training_amount in training_amount_list:
+        datapresetting = DataPreSetting(filename_qMC_Sobol, training_amount)
+        metamodel_validation_routine(
+            datapresetting, metamodelposttreatment, metamodelvalidation, "Kriging", training_amount
+        )
